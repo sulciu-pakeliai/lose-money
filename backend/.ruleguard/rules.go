5cf3c81ddc8f3badfefefcfff310ec81607f0977
CrynTox <88785107+CrynTox@users.noreply.github.com>
2026-04-30 11:02:12 +0300

    ci: define more general rule

--- backend/.ruleguard/rules.go
+++ backend/.ruleguard/rules.go
@@ -3,6 +3,7 @@ package gorules
 import "github.com/quasilyte/go-ruleguard/dsl"
 
 func avoidMagicTokenLength(m dsl.Matcher) {
-	m.Match(`mustRandomToken(24)`).
-		Report("avoid magic token length 24; use a named constant")
+	m.Match(`mustRandomToken($n)`).
+		Where(m["n"].Text.Matches(`^\d+$`)).
+		Report("avoid magic token length; use a named constant")
 }
